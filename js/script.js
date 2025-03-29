1fb710a741f42eef2f0fb638fe534ae3ac1204d8
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-03-29 04:44:23 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina. peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina. peighambarzadeh
+// Created on: mar 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle() {
+  // input
+  const baseOfTriangle = parseFloat(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseFloat(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (1 / 2) * baseOfTriangle * heightOfTriangle
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle.toFixed(2) + ' cm²'
+}
