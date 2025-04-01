543f6465868fa761f3d9ff7514b444c3e8afdfe6
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-04-01 16:22:51 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
 // Copyright (c) 2025 Adrina peighambarzadeh All rights reserved
 
-
 //
-
 
 // Created by: Adrina peighambarzadeh
 
-
 // Created on: Mar 2025
-
 
 // This file contains the JS functions for index.html
 
-
 "use strict"
-
 
 /**
 
@@ -24,35 +18,27 @@
 
 */
 
-
 function calculateAreaOfTriangle() {
   // input
-
 
   const aBaseOfTrapezoid = parseFloat(
     document.getElementById("a-base-of-trapezoid").value
   )
 
-
   const bBaseOfTrapezoid = parseFloat(
     document.getElementById("b-base-of-trapezoid").value
   )
-
 
   const heightOfTrapezoid = parseFloat(
     document.getElementById("height-of-trapezoid").value
   )
 
-
   // process
-
 
   const areaOfTrapezoid =
     ((aBaseOfTrapezoid + bBaseOfTrapezoid) / 2) * heightOfTrapezoid
 
-
   // output
-
 
   document.getElementById("answer").innerHTML =
     "Area of your Trapezoid is: " + areaOfTrapezoid + " mm²"
